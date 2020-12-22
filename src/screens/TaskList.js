7e21b2db46aa670ea6575962f508f3d1cdb179ab
willamy388720 <willamygt@gmail.com>
2020-12-22 17:33:52 -0300

    add task button

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native'
 
 import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
@@ -15,7 +23,7 @@ import AddTask from './AddTask'
 export default class TaskList extends Component {
   state = {
     showDoneTasks: true,
-    showAddTask: true,
+    showAddTask: false,
     visibleTasks: [],
     tasks: [
       {
@@ -87,6 +95,12 @@ export default class TaskList extends Component {
             renderItem={({ item }) => <Task {...item} toggleTask={this.toggleTask} />}
           />
         </View>
+        <TouchableOpacity style={styles.addButton}
+          activeOpacity={0.7}
+          onPress={() => this.setState({ showAddTask: true })}>
+          <Icon name="plus" size={20}
+            color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
       </View>
     )
   }
@@ -126,4 +140,15 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     marginTop: Platform.OS === 'ios' ? 40 : 10
   },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })

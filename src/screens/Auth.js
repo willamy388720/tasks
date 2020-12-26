280f85c5b45ab04db784b7408bab43fb1fd5f218
willamy388720 <willamygt@gmail.com>
2020-12-26 15:30:21 -0300

    creating validations for the form

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -18,8 +18,8 @@ import { server, showError, showSuccess } from '../common'
 
 const initialState = {
   name: '',
-  email: '',
-  password: '',
+  email: 'will@mail.com',
+  password: '123456',
   confirmPassword: '',
   stageNew: false
 }
@@ -67,6 +67,17 @@ export default class Auth extends Component {
   }
 
   render() {
+    const validations = []
+    validations.push(this.state.email && this.state.email.includes('@'))
+    validations.push(this.state.password && this.state.password >= 6)
+
+    if (this.state.stageNew) {
+      validations.push(this.state.name && this.state.name.trim().length >= 3)
+      validations.push(this.state.password === this.state.confirmPassword)
+    }
+
+    const validForm = validations.reduce((t, a) => t && a)
+
     return (
       <ImageBackground source={backgroundImage}
         style={styles.background}>
@@ -96,8 +107,10 @@ export default class Auth extends Component {
               style={styles.input} secureTextEntry={true}
               onChangeText={confirmPassword => this.setState({ confirmPassword })} />
           }
-          <TouchableOpacity onPress={this.signinOrSignup}>
-            <View style={styles.button}>
+          <TouchableOpacity onPress={this.signinOrSignup}
+            disabled={!validForm}
+          >
+            <View style={[styles.button, validForm ? {} : { backgroundColor: '#AAA' }]}>
               <Text style={styles.buttonText}>
                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
               </Text>

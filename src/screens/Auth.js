7da0c96c1e2a6ca08910a8daf3590c9e4c0dacc3
willamy388720 <willamygt@gmail.com>
2020-12-26 17:53:05 -0300

    user information

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -60,7 +60,7 @@ export default class Auth extends Component {
         password: this.state.password
       })
       axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-      this.props.navigation.navigate('Home')
+      this.props.navigation.navigate('Home', res.data)
     } catch (e) {
       showError(e)
     }
--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,11 +1,65 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, View, Text, StyleSheet } from 'react-native'
 import { DrawerItems } from 'react-navigation-drawer'
+import { Gravatar } from 'react-native-gravatar'
+import commonStyles from '../commonStyles'
 
 export default props => {
   return (
     <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.title}>Tasks</Text>
+        <Gravatar style={styles.avatar}
+          options={{
+            email: props.navigation.getParam('email'),
+            secure: true
+          }} />
+        <View style={styles.userInfo}>
+          <Text style={styles.name}>
+            {props.navigation.getParam('name')}
+          </Text>
+          <Text style={styles.email}>
+            {props.navigation.getParam('email')}
+          </Text>
+        </View>
+      </View>
       <DrawerItems {...props} />
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  header: {
+    borderBottomWidth: 1,
+    borderColor: '#DDD',
+  },
+  title: {
+    color: '#000',
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 30,
+    paddingTop: 30,
+    padding: 10
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderWidth: 3,
+    borderRadius: 30,
+    margin: 10,
+  },
+  userInfo: {
+    marginLeft: 10,
+  },
+  name: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+    color: commonStyles.colors.text,
+    marginBottom: 5,
+  },
+  email: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 15,
+    color: commonStyles.colors.subText,
+    marginBottom: 10,
+  }
+})
